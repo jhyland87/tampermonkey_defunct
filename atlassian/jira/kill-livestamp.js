1c3304ee875330d5affd6a49ac0c6d1a39bbb226
Justin Hyland <j@linux.com>
2017-03-22 09:25:01 -0700

    updates

--- atlassian/jira/kill-livestamp.js
+++ atlassian/jira/kill-livestamp.js
@@ -1,19 +1,33 @@
-/**
- * This changes the friendly timestamp display values in Atlassian from values like: Now, Today, Yesterday, n Hours Ago, etc
- * Then changes the <time> tags to <span> tags (since <time> tags get updated every couple seconds)
- */
-var $time = $( 'time' )
-if ( $time.length ) {
-  console.debug( 'Found %s <time> elements in DOM', $time.length )
+// ==UserScript==
+// @name          Kill Jira Livestamp
+// @namespace     https://raw.githubusercontent.com/jhyland87/tampermonkey/master/atlassian/jira/
+// @version       0.1
+// @description   This changes the friendly timestamp display values in Atlassian from values like: Now, Today, Yesterday, n Hours Ago, etc Then changes the <time> tags to <span> tags (since <time> tags get updated every couple seconds)
+// @author        Justin Hyland (j@linux.com)
+// @match         /^https?://.*\.atlassian\.com/
+// @homepage      https://github.com/jhyland87/tampermonkey
+// @grant         none
+// @run-at        document-start
+// @downloadURL   https://raw.githubusercontent.com/jhyland87/tampermonkey/master/atlassian/jira/kill-livestamp.js
+// @updateURL     https://raw.githubusercontent.com/jhyland87/tampermonkey/master/atlassian/jira/kill-livestamp.js
+// ==/UserScript==
+(function() {
+  'use strict';
 
-  $time.each(function( idx, elm ){
-    $( elm )
-      .text( new Date( $( elm ).attr( 'datetime' ) ).toGMTString() )
-      .replaceWith(function() { 
-        return '<span>' + this.innerHTML + '</span>'
-      })
-  })
-}
-else {
-  console.debug( 'No <time> elements found in DOM' )
-}
+  var $time = $( 'time' );
+  ;
+  if ( $time.length ) {
+    console.debug( 'Found %s <time> elements in DOM', $time.length );
+
+    $time.each(function( idx, elm ){
+      $( elm )
+        .text( new Date( $( elm ).attr( 'datetime' ) ).toGMTString() )
+        .replaceWith(function() { 
+          return '<span>' + this.innerHTML + '</span>';
+        });
+    });
+  }
+  else {
+    console.debug( 'No <time> elements found in DOM' );
+  }
+})();
